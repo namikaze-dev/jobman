3547c4cc425e119bc640775bd1058cdc86f3a419
namikaze-dev <usmansadik96@gmail.com>
2023-02-13 00:43:07 +0100

    feat: add getbyemail method to user model

--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const { Conflict } = require('../lib/errors/http_errors');
+const { Conflict, NotFound } = require('../lib/errors/http_errors');
 
 class UserModel {
     constructor(dbClient) {
@@ -23,6 +23,22 @@ class UserModel {
             if (err.code == '23505') {
                 throw new Conflict('user email conflict');
             } 
+            throw err;
+        }
+    }
+
+
+    async getByEmail(email) {
+        try {
+            const stmt = 'SELECT * FROM  users WHERE email = $1';
+            const result = await this.db.query(stmt, [email]);
+            
+            if (result.rowCount == 0) {
+                throw new NotFound()
+            }
+
+            return result.rows[0];
+        } catch (err) {
             throw err;
         }
     }
