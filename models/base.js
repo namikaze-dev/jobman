7ce0b135dfb89a8956ce8fc6dcd447fb04664177
namikaze-dev <usmansadik96@gmail.com>
2023-03-07 21:59:36 +0100

    feat: add method to insert new job

--- models/base.js
+++ models/base.js
@@ -1,12 +1,14 @@
 'use strict'
 
 const UserModel = require('./user');
-const { TokenModel } = require('./token')
+const { TokenModel } = require('./token');
+const JobModel = require("./job");
 
 class Models {
     constructor(db) {
         this.users = new UserModel(db);
         this.tokens = new TokenModel(db);
+        this.jobs = new JobModel(db);
     }
 }
 
---
+++ models/job.js
@@ -0,0 +1,32 @@
+'use strict'
+
+const { Conflict, BadRequest } = require("../lib/errors/http_errors");
+
+class JobModel {
+    constructor(dbClient) {
+        this.db = dbClient;
+    }
+
+    async insert(userId, job) {
+        try {
+            const result = await this.db.query(
+                `INSERT INTO jobs 
+                  (user_id, title, description, company_name, type, skills, location, remote, apply_url)
+                  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
+                  RETURNING *`,
+                [userId, job.title, job.description, job.company_name, job.type || 'fulltime',
+                  job.skills || null, job.location || null, job.remote || false, job.apply_url]
+            );
+
+            return result.rows[0];
+        } catch (err) {
+            if (err.code == '23502') {
+                throw new BadRequest('job missing required values')
+            }
+
+            throw err;
+        }
+    }
+}
+
+module.exports = JobModel;
