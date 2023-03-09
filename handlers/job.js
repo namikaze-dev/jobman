17e45dd5d09afcadb46d9054c2c4de9d750373f9
namikaze-dev <usmansadik96@gmail.com>
2023-03-09 22:16:54 +0100

    feat: add filter/pagination input parsing to getAll jobs handler

--- handlers/job.js
+++ handlers/job.js
@@ -169,7 +169,43 @@ const get = env => {
 const getAll = env => {
     return async (req, res) => {
         try {
-            const jobs = await env.models.jobs.getAll();
+            const input = {
+                title: req.query.title || "",
+                company_name: req.query.company_name || "",
+                company_market: req.query.market || "",
+                location: req.query.location || "",
+                skills: typeof req.query.skills == 'string' ? req.query.skills.split(",") : [],
+                type: req.query.type || "",
+                remote: req.query.remote || false,
+                // pagination & sort params
+                page: parseInt(req.query.page) || 1,
+                page_size: parseInt(req.query.page_size) || 10,
+                sort: req.query.sort || ""
+            }
+
+            const v = new Validator(input, {
+                title: 'maxLength:500',
+                company_name: 'maxLength:100',
+                type: 'in:fulltime,part-time,contract,internship,freelance',
+                company_market: 'maxLength:100',
+                skills: 'array',
+                'skills.*': 'string',
+                location: 'string',
+                remote: 'boolean',
+
+                page: 'integer|min:1|max:10000000',
+                page_size: 'integer|min:1|max:1000',
+                sort: 'string|in:id,title,company_name,-id,-title,-company_name'
+            });
+
+            if (!await v.check()) {
+                failedValidationResponse(res, sanitizer.validationErr(v.errors));
+                return;
+            }
+
+            console.log(input);
+
+            const jobs = await env.models.jobs.getAll(input);
 
             res.status(200).send(sanitizer.job({ jobs: jobs }))
         } catch (err) {
