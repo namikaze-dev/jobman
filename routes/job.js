ee55527900650e7302f8ed898c026dbc36a83957
namikaze-dev <usmansadik96@gmail.com>
2023-03-11 14:45:20 +0100

    fix: resolve route conflict

--- routes/job.js
+++ routes/job.js
@@ -7,13 +7,14 @@ const { authenticate, activate } = require('../lib/middlewares')
 const route = env => {
     const router = Router();
 
+    router.get("/jobs/subscriptions", subscribe(env));
+    router.post("/jobs/payment-intent", createPaymentIntent(env));
+
     router.get("/jobs/:id", get(env));
     router.get("/jobs", getAll(env));
     router.post("/jobs", authenticate(env), activate(env), create(env));
     router.put("/jobs/:id", authenticate(env), activate(env), update(env));
     router.delete("/jobs/:id", authenticate(env), activate(env), remove(env));
-    router.post("/jobs/payment-intent", createPaymentIntent(env));
-    router.get("/jobs/subscriptions", subscribe(env));
 
     return router;
 }
